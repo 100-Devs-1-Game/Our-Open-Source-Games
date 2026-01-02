4f89ebac8471d4667a8b0d0d24f1fee226813aaa
Griiimon <25064539+Griiimon@users.noreply.github.com>
2026-01-02 12:02:26 +0100

    Create build_readme.py

---
+++ build_readme.py
@@ -0,0 +1,19 @@
+import glob
+import yaml
+
+OUT = ["# Our Games\n"]
+
+for path in sorted(glob.glob("items/*.yaml")):
+    with open(path) as f:
+        g = yaml.safe_load(f)
+
+    OUT.append(f"## {g['title']}\n")
+    OUT.append(f"{g['description']}\n\n")
+    OUT.append(f"![Screenshot]({g['screenshot']})\n\n")
+    OUT.append(f"[Repo]({g['repo']}) | [itch.io]({g['itch']}) | [Discord]({g['discord']})")
+    if 'gdd' in g and g['gdd']:
+      OUT.append(f" | [Game Design Document]({g['gdd']})")
+    OUT.append("\n\n")
+
+with open("README.md", "w") as f:
+    f.write("\n".join(OUT))
